ea91e2c867b6de41cebbb56689c8b20e7ac224cb
Rojean Lin <rojean@Rojeans-MacBook-Air.local>
2025-01-20 17:16:27 +0800

    hw01_first_submission

--- student_assignment.py
+++ student_assignment.py
@@ -5,12 +5,67 @@ from model_configurations import get_model_configuration
 
 from langchain_openai import AzureChatOpenAI
 from langchain_core.messages import HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, FewShotChatMessagePromptTemplate
 
 gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
 
+llm = AzureChatOpenAI(
+        model=gpt_config['model_name'],
+        deployment_name=gpt_config['deployment_name'],
+        openai_api_key=gpt_config['api_key'],
+        openai_api_version=gpt_config['api_version'],
+        azure_endpoint=gpt_config['api_base'],
+        temperature=gpt_config['temperature']
+)
+# ChatPromptTemplate usage test 
+non_formatted_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("human", "please provide the result of : {input} into json in text format, no need .md format tag, without any other explaination"),
+    ]
+)
+
+# for hw01 
+# FewShotChatMessagePromptTemplate
+example_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("human", "以json格式，純文字形式列出全部結果：{input}, 不必說明"),
+        ("ai", "{output}")
+    ]
+)
+
+examples = [
+    {"input": "2024年台灣10月紀念日有哪些?", 
+     "output": """
+     {
+        "Result": [
+          {
+               "date": "2024-10-10",
+               "name": "國慶日"
+          }
+        ]
+    }
+    """
+    },
+]
+
+# A prompt template used to format each individual example.
+few_shot_prompt = FewShotChatMessagePromptTemplate(
+    examples=examples,
+    example_prompt=example_prompt,
+)
+
+# Add into ChatPromptTemplate for result
+holiday_prompt = ChatPromptTemplate.from_messages(
+    [
+        few_shot_prompt,
+        ('human', '{input}'),
+    ]
+)
+
 def generate_hw01(question):
-    pass
+    prompt_value = holiday_prompt.format(input=question)
+    return llm.invoke(prompt_value).content
     
 def generate_hw02(question):
     pass
